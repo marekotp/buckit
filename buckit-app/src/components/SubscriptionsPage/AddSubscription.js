6a1eab7ee1d37a0360c566d6bce79fc69022c314
DESKTOP-N1HG3HE\Marek <marekotp@gmail.com>
2023-03-24 01:08:06 +1300

    commit

---
+++ buckit-app/src/components/SubscriptionsPage/AddSubscription.js
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import '../../App.css'
+
+import { Container, Button } from '@mui/material';
+
+export default function AddSubscription() {
+
+  return (
+    <div>
+      <Container>
+        <Button variant="contained">Add Subscription</Button>
+      </Container>
+    </div>
+  );
+}
